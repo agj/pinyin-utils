ca80ecd1a65594b8135608029c542468aa3937cc
agj <ale@agj.cl>
2018-01-07 23:51:52 -0300

    Fix bugs dealing with neutral tone

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const codepointToUnicode = (codepoint) => {
 	if (typeof codepoint === 'string') {
 		codepoint = codepoint.replace('U+', '')
-		
+
 		if (!/^0x/.test(codepoint)) {
 			codepoint = '0x' + codepoint
 		}
@@ -26,76 +26,57 @@ const vovels = {
 	"ü": ['ǖ', 'ǘ', 'ǚ', 'ǜ']
 }
 
+const toneNumberRegex = /[a-zü](\d)/;
 const getToneNumber = (text) => {
 	text = text.toLowerCase()
 
-	let tone = 0
+	if (toneNumberRegex.test(text)) {
+		return parseInt(text.match(toneNumberRegex)[1])
+	}
 
 	for (let v in vovels) {
 		for (var i = 0; i < vovels[v].length; i++) {
-			const t = i + 1
 			if (text.match(vovels[v][i])) {
-				tone = t
-				break
-			}
-			if (text.match(new RegExp('[a-zü]+'+t))) {
-				tone = t
-				break
+				return i + 1
 			}
 		}
 	}
 
-	return tone
+	return 5
 }
 
 const removeTone = (text) => {
-	let removed = false
-
 	// remove tone from pinyin with tone marks
-	if (getToneNumber(text) > 0) {
-		for (let i in vovels) {
-			for (let t of vovels[i]) {
-				if (text.match(t)) {
-					text = text.replace(t, i)
-					removed = true
-				}
+	for (let i in vovels) {
+		for (let t of vovels[i]) {
+			if (text.match(t)) {
+				text = text.replace(t, i)
 			}
 		}
 	}
 
 	// remove tone from pinyin with tone numbers
-	const matches = text.match(/[1-4]/)
-	if (matches && matches.length > 0) {
-		text = text.replace(matches[0], '')
-		removed = true
-	}
+	text = text.replace(/\d/g, '');
 
-	return removed && text
+	return text
 }
 
 const markToNumber = (text) => {
 	const tone = getToneNumber(text)
 
-	if (tone > 0) {
-		for (let v in vovels) {
-			for (var t = 0; t < vovels[v].length; t++) {
-				if (text.match(vovels[v][t])) {
-					text = text.replace(vovels[v][t], v)
-				}
-			}
-		}
-		text += tone
+	if (tone !== 5) {
+		return removeTone(text) + tone
 	}
 
-	return text	
+	return text
 }
 
 const numberToMark = (text) => {
 	const tone = getToneNumber(text)
 
-	if (tone > 0) {
-		text = text.replace(/[1-4]/, '')
+	text = text.replace(/\d/g, '')
 
+	if (tone !== 5) {
 		const matchedVovels = text.match(/[aeiouü]/g)
 		if (matchedVovels) {
 			let vovel = matchedVovels[matchedVovels.length-1]
